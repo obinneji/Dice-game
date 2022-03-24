0973609ef93e5f7ca625937ba89c0bf38a142abf
obinneji <obinnejic@gmail.com>
2022-03-24 01:50:02 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const rollDice = document.querySelector("button")
+
+rollDice.addEventListener("click", function(){
+    let randomNumber1 = Math.floor(Math.random()* 6) + 1;
+    let randomDiceImage1 = "dice" + randomNumber1 + ".png";
+    let randomImageSource1 = "images/" + randomDiceImage1
+    let image1 = document.querySelectorAll("img")[0]
+    image1.setAttribute("src", randomImageSource1 )
+
+    let randomNumber2 = Math.floor(Math.random()* 6) + 1;
+    let randomImageSource2= "images/dice" + randomNumber2 +".png"
+    let image2 = document.querySelectorAll("img")[1]
+    image2.setAttribute("src", randomImageSource2 )
+
+    if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "😎Play 1 wins"
+    }else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Play 2 wins😎"
+    }else {
+    document.querySelector("h1").innerHTML = "Draw😶"
+    }   
+})
+
+
+
